7c0eaa0a9604a789445acab1fce6c17cb74286fc
rorro6787 <emiliorodrigo.ecr@gmail.com>
2024-09-30 14:15:35 +0200

    Frontend structure and basic app designed

---
+++ src/frotend/astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config'
+import tailwind from "@astrojs/tailwind"
+
+import robotsTxt from "astro-robots-txt"
+import react from '@astrojs/react';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), robotsTxt(), react()],
+  site: 'https://porfolio.dev/'
+})
